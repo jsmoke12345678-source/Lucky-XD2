dacddf79234b2281a104c0bab90ef490711d1d99
jsmoke12345678-source <jsmoke12345678@gmail.com>
2026-05-03 16:53:38 +0300

    Update settings.js
    New changes

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: settings.SESSION_ID || process.env.SESSION_ID || "",
+  SESSION_ID: settings.SESSION_ID || process.env.SESSION_ID || "starcore~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0krTHltblN0Tzk4R05BVU1zKzJsYXdCQTY0ellmTnI5aCtZSUdZR1Qyaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSTVtV09udFdneE9oNGxPRjZIV0Z5WDR6Wi9CWWNhVjJuYUpEUGNOMkMyQT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjSFc4SmljQVgzNkZrUGQ3cmV5MVVMRmk4VWc0QzV0eXc4dmtVbmFudkd3PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJY2x0M2xXMEgvT2hKZnoyNE9tQjQ4b0JJQVVZRE5KNVdUUXI0T0JiU2lNPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlVNdmFObmNmYUViVjFCTHpZQ1F4Y3pSbjNSRlg0aXhjRVFPaVNlZVpSV2M9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ilk3eHhiMFFNMWFFSGNTYkl2YXJVVVF0bTZhOVhJMFNUakNwMlRmK3pYaGM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkRvTVhzODdSdXFjQ0Q4YW9xRmcxcktsbXUzeVh0K3FDQXpTelNyUlQycz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib25YeXRZWWNOSU4rS0pBeC9Temt6V3lhNjVZd01UNlhWc0VLcmxhSTZDMD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImRJc3pHQ1BhQnJYSG1vNzR6TTd6MUYrMStPcTAxQkNVcm9jNUdlL1dOcjlGd2xxN05PcU9uYmJSSzJpdUN3clp5ekZQTHEwbk0rT0JRQ1NPMUNJd2lnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjI0LCJhZHZTZWNyZXRLZXkiOiJza2tJN3dzbVhUN0tlL0ZTZE54V21vZzNlMVluUTFBZUpIcTdLbkFnTjZ3PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJDZlNFVFM0eFJZYW4zWnZxeS0ydmt3IiwicGhvbmVJZCI6IjljNDE2YjZlLTBmNWQtNGFhMC04NGZiLTcyMTVjMWJmZjAwNSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrR1NHVWxtRTRlelZxcFVlU1RWQUFxUzNHZTA9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZERMSGN0N2ZHVWZuZlM2L3dISzFWZC9velpjPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ik1STUFMVklOIiwibWUiOnsiaWQiOiIyNTY3MDc3NzA4MDg6M0BzLndoYXRzYXBwLm5ldCIsImxpZCI6IjE5NDc1MTcwMTc0MTc4OTozQGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTERkaGZNQkVPZUQzYzhHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiUTkzWVNmS1FGZis0d3FRT3JSbmV6MWwyWjV0K24xcXVKWFZVeSsyTDUyVT0iLCJhY2NvdW50U2lnbmF0dXJlIjoieFdrSmVDOHlWQ2NrVURSbUlGdnhNT2NmdnNmREV0Z0hrQWFnSXJrTlBkN0lZTHhHcVVwWGp1MDJoQ0JPayt4VCs5S25NbUx0YWdqb1IrN0FWeVk3Q2c9PSIsImRldmljZVNpZ25hdHVyZSI6IjA1N0tIdzdER2tIK0ZvclVOZ1RlWnhTWnZZbG5jeXVPak8rVnphWEF1WWlkT1VxY1ZZSGlidklVOVVlejUrUUJhdmM4SENJczh4VzE4dStnQ0JpbmlnPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU2NzA3NzcwODA4OjNAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVVBkMkVueWtCWC91TUtrRHEwWjNzOVpkbWViZnA5YXJpVjFWTXZ0aStkbCJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FrSUF3Z1MifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzc3ODExOTYwLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUFURSJ9",
   PREFIX: getConfig("PREFIX") || "." || settings.PREFIX,
   CHATBOT: getConfig("CHATBOT") || "on",
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ʟᴜᴄᴋʏ-xᴅ",
@@ -24,34 +24,34 @@ module.exports = {
   BAILEYS: process.env.BAILEYS || "@whiskeysockets/baileys",
 
   // ===== OWNER & DEVELOPER SETTINGS =====
-  OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "256789966218",
-  OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "ʟᴜᴄᴋʏ ➋➊➑",
+  OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "256707770807",
+  OWNER_NAME: process.env.OWNER_NAME || getConfig("jsmoke") || "ʟᴜᴄᴋʏ ➋➊➑",
   DEV: process.env.DEV || "256789966218",
   DEVELOPER_NUMBER: '256789966218@s.whatsapp.net',
   MENU_AUDIO_URL: process.env.MENU_AUDIO_URL || 'https://files.catbox.moe/3v5i11.mp3',
 NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363420656466131@newsletter',
 
   // ===== AUTO-RESPONSE SETTINGS =====
-  AUTO_REPLY: process.env.AUTO_REPLY || "false",
+  AUTO_REPLY: process.env.AUTO_REPLY || "true",
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
   AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*Just seen ur status 😆 🤖*",
-  READ_MESSAGE: process.env.READ_MESSAGE || "false",
+  READ_MESSAGE: process.env.READ_MESSAGE || "true",
   REJECT_MSG: process.env.REJECT_MSG || "*📵 Calls are not allowed on this number unless you have permission. 🚫*",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4itzeu.jpg",
   LIVE_MSG: process.env.LIVE_MSG || "> ʙᴏᴛ ɪs sᴘᴀʀᴋɪɴɢ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ᴀʟɪᴠᴇ\n\n\nᴋᴇᴇᴘ ᴜsɪɴɢ ✦ʟᴜᴄᴋʏ xᴅ✦ ғʀᴏᴍ ʟᴜᴄᴋʏ ᴛᴇᴄʜ ʜᴜʙ  ɪɴᴄ⚡\n\n\n*© ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ\n\n> ɢɪᴛʜᴜʙ :* github.com/Tomilucky218/Lucky-XD2",
 
   // ===== REACTION & STICKER SETTINGS =====
-  AUTO_REACT: process.env.AUTO_REACT || "false",
-  OWNER_REACT: process.env.OWNER_REACT || "false",
-  CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+  AUTO_REACT: process.env.AUTO_REACT || "true",
+  OWNER_REACT: process.env.OWNER_REACT || "true",
+  CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
   CUSTOM_REACT_EMOJIS: getConfig("CUSTOM_REACT_EMOJIS") || process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
   STICKER_NAME: process.env.STICKER_NAME || "ᴋʜᴀɴ-ᴍᴅ",
-  AUTO_STICKER: process.env.AUTO_STICKER || "false",
+  AUTO_STICKER: process.env.AUTO_STICKER || "true",
 
   // ===== MEDIA & AUTOMATION =====
-  AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
-  AUTO_TYPING: process.env.AUTO_TYPING || "false",
-  MENTION_REPLY: process.env.MENTION_REPLY || "false",
+  AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+  AUTO_TYPING: process.env.AUTO_TYPING || "true",
+  MENTION_REPLY: process.env.MENTION_REPLY || "truee",
   MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://files.catbox.moe/4itzeu.jpg",
 
   // ===== SECURITY & ANTI-FEATURES =====
@@ -68,13 +68,13 @@ NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363420656466131@newsletter',
   // ===== BOT BEHAVIOR & APPEARANCE =====
   DESCRIPTION: process.env.DESCRIPTION || "*© Powered By Lucky Tech Hub*",
   PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE |true",
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-  AUTO_BIO: process.env.AUTO_BIO || "false",
-  WELCOME: process.env.WELCOME || "false",
-  GOODBYE: process.env.GOODBYE || "false",
-  ADMIN_ACTION: process.env.ADMIN_ACTION || "false",
+  AUTO_BIO: process.env.AUTO_BIO || "true",
+  WELCOME: process.env.WELCOME || "true",
+  GOODBYE: process.env.GOODBYE || "true",
+  ADMIN_ACTION: process.env.ADMIN_ACTION || "true",
   version: process.env.version || "1.5.0",
   TIMEZONE: settings.TIMEZONE || process.env.TIMEZONE || "Africa/Kampala",
 
